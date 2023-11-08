1278a7f1b2feb3d569434387c910f1354da08f99
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-08 13:02:51 +0300

    Добавьте файлы проекта.

---
+++ Skymey-stocks-polygon/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Skymey_stocks_polygon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
